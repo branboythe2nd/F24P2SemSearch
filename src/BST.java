e4101d6d6c6a309a4e3693a7d3a49e8567c9a20b
branboy <brantsonbui@vt.edu>
2024-09-26 15:57:54 -0400

    insert and delete

--- src/BST.java
+++ src/BST.java
@@ -9,18 +9,19 @@ public class BST<T extends Comparable<T>> {
     }
 
 
-    public void searchExact(T value) {
+    public Node<T> searchExact(T value) {
+        return null;
+    }
+    
 
+
+    public Node<T> searchRange(T lower, T higher) {
+        return null;
     }
 
 
-    public void searchRange(T lower, T higher) {
-
-    }
-
-
-    public void searchLocation(int x, int y, int radius) {
-
+    public Node<T> searchLocation(int x, int y, int radius) {
+        return null;
     }
 
 
@@ -30,7 +31,7 @@ public class BST<T extends Comparable<T>> {
     }
 
 
-    public Node<T> insertHelp(Node<T> root, T key, Seminar value) {
+    private Node<T> insertHelp(Node<T> root, T key, Seminar value) {
         if (root == null) {
             root = new Node<T>(key, value, null, null);
             return root;
@@ -47,8 +48,45 @@ public class BST<T extends Comparable<T>> {
     }
 
 
-    public void delete(int ID) {
+    public void delete(T key) {
+        root = deleteHelp(root, key);
+        numOfRecords--;
+    }
 
+
+    private Node<T> deleteHelp(Node<T> root, T key) {
+        if (root == null) {
+            return null;
+        }
+        int out = root.getData().compareTo(key);
+        if (out > 0) {
+            root.setLeft(deleteHelp(root.getLeft(), key));
+        }
+        else if (out < 0) {
+            root.setRight(deleteHelp(root.getRight(), key));
+        }
+        else {
+            if (root.getLeft() == null) {
+                return root.getRight();
+            }
+            else if (root.getRight() == null) {
+                return root.getLeft();
+            }
+            else {
+                Node<T> max = findMax(root.getLeft());
+                root.setData(max.getData());
+                root.setLeft(deleteHelp(root.getLeft(), max.getData()));
+            }
+        }
+        return root;
+    }
+
+
+    private Node<T> findMax(Node<T> root) {
+        while (root.getRight() != null) {
+            root = root.getRight();
+        }
+        return root;
     }
 
 
