08b59ada0a14e6e2181b0cffd60d8053cffcbda6
adarshchitti <149348199+adarshchitti@users.noreply.github.com>
2024-10-01 15:37:43 -0400

    drfghj

---
+++ src/BSTTest.java
@@ -0,0 +1,16 @@
+
+
+public class BSTTest extends student.TestCase {
+    
+    private BST<Integer> test;
+    private BST<String> toTest;
+    
+    public void setUp()
+    {
+       test = new BST<Integer>();
+       toTest = new BST<String>();        
+    }
+    
+    
+
+}
